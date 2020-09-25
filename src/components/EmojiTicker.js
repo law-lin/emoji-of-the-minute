16ee6b46d2f3570be9dba2a2d6b62970b570a89d
law-lin <lawrencelin101@gmail.com>
2020-09-25 19:47:23 -0400

    dependencies and emoji ticker

---
+++ src/components/EmojiTicker.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Ticker from "react-ticker";
+function EmojiTicker() {
+  return <Ticker>Hello</Ticker>;
+}
+
+export default EmojiTicker;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import EmojiTicker from "./EmojiTicker";
 
 function HomePage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <EmojiTicker />
+    </div>
+  );
 }
 
 export default HomePage;
